89e8938cca25eac5bdc992dfcf089f11ace6ad6e
Luis Yallico <yallico92@gmail.com>
2024-10-26 16:13:11 +0100

    define frames  for part 1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# robotic_fundamentals
-For RF Coursework
+# Robotic Fundamentals
+Coursework
+
+## Report
+
+Use the following link: [overleaf document](https://www.overleaf.com/7357247853vyvvffbsbkvq#4c8046)
---
+++ src/plot_oriented_cylinder.m
@@ -0,0 +1,32 @@
+function plot_oriented_cylinder(radius, height, origin, orientation)
+    % create a unit cylinder along the Z-axis
+    [X, Y, Z] = cylinder(radius);
+    Z = Z * height;
+
+    % make rotation matrix from orientation
+    R = eul2rotm(orientation); % assuming orientation is in Euler angles
+
+    % X, Y, Z matrices into lists of 3D points
+    points = [X(:), Y(:), Z(:)]';
+
+    % Rotate all points in a single matrix operation
+    rotated_points = R * points;
+
+    % Translate points by adding the origin
+    X_rot = reshape(rotated_points(1, :) + origin(1), size(X));
+    Y_rot = reshape(rotated_points(2, :) + origin(2), size(Y));
+    Z_rot = reshape(rotated_points(3, :) + origin(3), size(Z));
+
+    % Plot the transformed cylinder
+    surf(X_rot, Y_rot, Z_rot, 'EdgeColor', 'none', 'FaceColor', [0.6, 1.0, 0.6]);
+
+    % edge top and bottom circular end caps with black edge
+    % bottom cap
+    patch(X_rot(1, :), Y_rot(1, :), Z_rot(1, :), [0.6, 1.0, 0.6], 'EdgeColor', 'k', 'LineWidth', 0.5);
+    % top cap
+    patch(X_rot(2, :), Y_rot(2, :), Z_rot(2, :), [0.6, 1.0, 0.6], 'EdgeColor', 'k', 'LineWidth', 0.5);
+
+
+    hold on;
+
+end
