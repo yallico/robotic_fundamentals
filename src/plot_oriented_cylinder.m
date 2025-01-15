9d1995ced4fd8080f4256def477e0d546f0e3f27
Luis Yallico <yallico92@gmail.com>
2025-01-15 10:29:04 +0000

    parallel update

--- src/plot_oriented_cylinder.m
+++ src/plot_oriented_cylinder.m
@@ -2,22 +2,18 @@ function plot_oriented_cylinder(radius, height, origin, orientation)
     % create a unit cylinder along the Z-axis
     [X, Y, Z] = cylinder(radius);
     Z = Z * height;
-
-    % make rotation matrix from orientation
     R = eul2rotm(orientation); % assuming orientation is in Euler angles
 
-    % X, Y, Z matrices into lists of 3D points
+    %X, Y, Z matrices into lists of 3D points
     points = [X(:), Y(:), Z(:)]';
 
-    % Rotate all points in a single matrix operation
+    %Rotate all points in a single matrix operation
     rotated_points = R * points;
 
-    % Translate points by adding the origin
     X_rot = reshape(rotated_points(1, :) + origin(1), size(X));
     Y_rot = reshape(rotated_points(2, :) + origin(2), size(Y));
     Z_rot = reshape(rotated_points(3, :) + origin(3), size(Z));
 
-    % Plot the transformed cylinder
     surf(X_rot, Y_rot, Z_rot, 'EdgeColor', 'none', 'FaceColor', [0.6, 1.0, 0.6]);
 
     % edge top and bottom circular end caps with black edge
@@ -25,7 +21,6 @@ function plot_oriented_cylinder(radius, height, origin, orientation)
     patch(X_rot(1, :), Y_rot(1, :), Z_rot(1, :), [0.6, 1.0, 0.6], 'EdgeColor', 'k', 'LineWidth', 0.5);
     % top cap
     patch(X_rot(2, :), Y_rot(2, :), Z_rot(2, :), [0.6, 1.0, 0.6], 'EdgeColor', 'k', 'LineWidth', 0.5);
-
 
     hold on;
 
